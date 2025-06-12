3e07996e86a2195b031d7b3879df7f1fcac73576
Woojin <jwj3171@gmail.com>
2025-06-12 18:08:15 +0900

    #3 feat(LandingPage) : Implement FetchTable

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,22 @@
-function LandingPage() {
-  return;
-}
+// src/pages/LandingPage/LandingPage.jsx
+import FetchTable from "../../components/FetchTable/FetchTable.jsx";
+import { invInitialData } from "../../config/invInitialData_v2";
 
-export default LandingPage;
+const columns = [
+  { label: "순위", key: "rank" },
+  { label: "기업명", key: "companyName" },
+  { label: "기업 소개", key: "description" },
+  { label: "카테고리", key: "category" },
+  { label: "누적 투자 금액", key: "totalInvestment" },
+  { label: "매출액", key: "revenue" },
+  { label: "고용 인원", key: "employees" },
+];
+
+export default function LandingPage() {
+  return (
+    <div className="startup-page">
+      <h2>전체 스타트업 목록</h2>
+      <FetchTable data={invInitialData} columns={columns} />
+    </div>
+  );
+}
