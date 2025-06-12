c774b2ef92a5a5afa50a0e0a87700b15d91b64bc
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-06-12 13:26:17 +0900

    기본 폴더 구조 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import MyCompanyCompare from "./pages/MyCompanyCompare/MyCompanyCompare.jsx";
+import MyCompanyResult from "./pages/MyCompanyResult/MyCompanyResult.jsx";
+import InvenstmentOverview from "./pages/InvestmentOverview/InvestmentOverview.jsx";
+import CompanyDetail from "./pages/CompanyDetail/CompanyDetail.jsx";
+import SelectedOverivew from "./pages/SelectedOverivew/SelectedOverivew.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/mycompanycompare" element={<MyCompanyCompare />} />
+        <Route path="/mycompanyresult" element={<MyCompanyResult />} />
+        <Route path="/investmentoverview" element={<InvenstmentOverview />} />
+        <Route path="/companydetail" element={<CompanyDetail />} />
+        <Route path="/selectedoverview" element={<SelectedOverivew />} />
+      </Routes>
     </div>
   );
 }
