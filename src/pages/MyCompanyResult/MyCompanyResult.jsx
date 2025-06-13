b9ef8aab0b9c1b8145c5189f44796ca27dedd8c0
dudska12 <skadud0113@naver.com>
2025-06-13 11:42:09 +0900

    #6 커스텀버튼 제작 및 기본UI 구현

--- src/pages/MyCompanyResult/MyCompanyResult.jsx
+++ src/pages/MyCompanyResult/MyCompanyResult.jsx
@@ -1,5 +1,39 @@
+//흠 뭐해야할까 일단 기본적인 틀만들어볼까? ..
+import MyCompanySection from "../../components/MyCompanySection/MyCompanySection";
+import FetchTable from "../../components/FetchTable/FetchTable";
+import { invInitialData } from "../../config/invInitialData_v2";
+import SelectOption from "../../components/SelectOption/selectOption";
+import CustomButton from "../../components/customTag/customButton/customButton";
+
+const columns = [
+  { label: "기업명", key: "companyName" },
+  { label: "기업 소개", key: "description" },
+  { label: "카테고리", key: "category" },
+  { label: "누적 투자 금액", key: "totalInvestment" },
+  { label: "매출액", key: "revenue" },
+  { label: "고용 인원", key: "employees" },
+];
+
 function MyCompanyResult() {
-  return;
+  return (
+    <div>
+      <div>
+        <CustomButton>다른기업비교하기</CustomButton>
+        <MyCompanySection></MyCompanySection>
+      </div>
+      <div>
+        <span>비교 결과 확인하기</span>
+        <SelectOption></SelectOption>
+        <FetchTable data={invInitialData} columns={columns} />
+      </div>
+      <div>
+        <span>기업 순위 확인하기</span>
+        <SelectOption></SelectOption>
+        <FetchTable data={invInitialData} columns={columns} />
+        <CustomButton>나의 기업에 투자하기</CustomButton>
+      </div>
+    </div>
+  );
 }
 
 export default MyCompanyResult;
