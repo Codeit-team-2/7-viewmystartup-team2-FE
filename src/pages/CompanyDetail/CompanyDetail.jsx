17968bc0622a0c693eed56dd5896109a18b63f60
T.hong <t.1021.hong@gmail.com>
2025-06-15 16:17:47 +0900

    페이지네이션 훅 + 컴포넌트 작성 #19

--- src/pages/CompanyDetail/CompanyDetail.jsx
+++ src/pages/CompanyDetail/CompanyDetail.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { invInitialData } from "../../config/invInitialData_v2.js";
+import { investorsListData } from "../../config/investorsListData.js";
 import { useParams } from "react-router-dom";
 import { DetailCompanyTitle } from "../../components/DetailCompany/DetailCompanyTitle.jsx";
 import { DetailCompanyList } from "../../components/DetailCompany/DetailCompanyList.jsx";
 import { DetailCompanyInfor } from "../../components/DetailCompany/DetailCompanyInfor.jsx";
 import InvestmentForm from "../../components/InvestmentForm/InvestmentForm.jsx";
+import InvestorTable from "../../components/DetailCompany/InvestorTable.jsx";
+import { usePagination } from "../../hooks/usePagination.js";
+import PaginationBtn from "../../components/DetailCompany/PaginationBtn.jsx";
 
 function CompanyDetail() {
   const { id } = useParams();
   const company = invInitialData.find(c => c.id === Number(id));
+  const companyId = company.id;
+  const pageSize = 5;
+  const [page, setPage] = useState(1);
+  const totalCount = (investorsListData[companyId] || []).length;
+
+  const { pageNumbers, hasPrev, hasNext, handlePageChange } = usePagination({
+    page,
+    setPage,
+    totalCount,
+    pageSize,
+  });
 
   return (
     <div>
@@ -16,6 +31,14 @@ function CompanyDetail() {
       <DetailCompanyList company={company} />
       <DetailCompanyInfor company={company} />
       <InvestmentForm company={company} />
+      <InvestorTable companyId={companyId} page={page} pageSize={pageSize} />
+      <PaginationBtn
+        page={page}
+        pageNumbers={pageNumbers}
+        hasPrev={hasPrev}
+        hasNext={hasNext}
+        handlePageChange={handlePageChange}
+      />
       {/* 앱에서 받은 투자  */}
       {/* 기업투자하기 버튼 */}
       {/* 투자한 사람 목록 */}
