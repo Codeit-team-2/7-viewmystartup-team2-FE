1ff870971182bd337b910b0b5b6dd1de890a863c
Woojin <jwj3171@gmail.com>
2025-06-15 21:45:58 +0900

    #8 refactor(NavBar): Change Link path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
         <NavLink to="/myEntList" className="navbarPage">
           나의 기업 비교
         </NavLink>
-        <NavLink to="/comparisonList" className="navbarPage">
+        <NavLink to="/selectedoverview" className="navbarPage">
           비교 현황
         </NavLink>
         <NavLink to="/investmentoverview" className="navbarPage">
