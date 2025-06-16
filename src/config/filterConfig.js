4133a1c605dae592683048e17041241de08d2449
dudska12 <skadud0113@naver.com>
2025-06-16 16:51:14 +0900

    #6 필터 및 모달창 컴포넌트 코드개선 후  result페이지에 적용완료

---
+++ src/config/filterConfig.js
@@ -0,0 +1,8 @@
+export const resultOptionsData = [
+  { value: "investment_desc", label: "누적 투자금액 높은순" },
+  { value: "investment_asc", label: "누적 투자금액 낮은순" },
+  { value: "revenue_desc", label: "매출액 높은순" },
+  { value: "revenue_asc", label: "매출액 낮은순" },
+  { value: "employment_desc", label: "고용 인원 높은순" },
+  { value: "employment_asc", label: "고용 인원 낮은순" },
+];
