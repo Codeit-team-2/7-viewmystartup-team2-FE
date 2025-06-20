4ddaceeed1aecb90640be8153f6ff6179c26f987
dudska12 <skadud0113@naver.com>
2025-06-20 11:19:22 +0900

    ref:기존 select를 custom 컴포로 교체

--- src/components/SelectOption/selectOption.jsx
+++ src/components/SelectOption/selectOption.jsx
@@ -1,19 +1,44 @@
-import style from "./selectOption.module.css";
-import React from "react";
-function SelectOption({ options, onChange }) {
+// CustomSelectOption.jsx
+import React, { useState } from "react";
+import styles from "./selectOption.module.css";
+import filtericon from "../../assets/filtericon.png";
+
+function CustomSelectOption({ options, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleSelect = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+    onChange({ target: { value: option.value } }); // 기존 onChange 형태 유지
+  };
+
   return (
-    <div>
-      <select className={style.selectBox} onChange={onChange}>
-        {options.map((option) => {
-          return (
-            <option key={option.value} value={option.value}>
+    <div className={styles.selectBoxWrapper}>
+      <div
+        className={styles.selected}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {selected.label}
+        <img className={styles.filtericon} src={filtericon} />
+      </div>
+      {isOpen && (
+        <ul className={styles.optionsList}>
+          {options.map((option, idx) => (
+            <li
+              key={option.value}
+              className={`${styles.optionItem} ${
+                option.divider ? styles.divider : ""
+              }`}
+              onClick={() => handleSelect(option)}
+            >
               {option.label}
-            </option>
-          );
-        })}
-      </select>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
-export default SelectOption;
+export default CustomSelectOption;
