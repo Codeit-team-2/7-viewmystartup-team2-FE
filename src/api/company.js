93fed305957c065406cd4b069459a9f7fe715a03
Woojin <jwj3171@gmail.com>
2025-06-20 13:03:49 +0900

    fix(LandingPage):sort&order&keyword#3

--- src/api/company.js
+++ src/api/company.js
@@ -1,10 +1,19 @@
 // src/api/company.js
 import axios from "axios";
 
+//기존 키워드 없이 다가져오는 부분 삭제예정
 export const fetchAllCompanies = (sortBy = "revenue", order = "desc") => {
   return axios
     .get("http://localhost:3000/companies", {
       params: { sortBy, order },
+    })
+    .then((res) => res.data);
+};
+
+export const fetchFilteredDataWJ = (keyword, sortBy = "revenue", order = "desc") => {
+  return axios
+    .get("http://localhost:3000/companies", {
+      params: { keyword, sortBy, order },
     })
     .then((res) => res.data);
 };
